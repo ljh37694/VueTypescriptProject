40628417275bc1ec675ff9804704270f1ffc2706
i_junghoon <ljh37694@naver.com>
2024-05-11 22:20:27 +0900

    테스트를 위한 투두 리스트 데이터

---
+++ to-vue/src/data.ts
@@ -0,0 +1,35 @@
+interface TodoType {
+  content: String,
+  createdDate: Date,
+  done: Boolean,
+}
+
+const data: TodoType[] = [
+  {
+    content: "내용1",
+    createdDate: new Date(),
+    done: false,
+  },
+  {
+    content: "내용1",
+    createdDate: new Date(),
+    done: false,
+  },
+  {
+    content: "내용1",
+    createdDate: new Date(),
+    done: false,
+  },
+  {
+    content: "내용1",
+    createdDate: new Date(),
+    done: false,
+  },
+  {
+    content: "내용1",
+    createdDate: new Date(),
+    done: false,
+  }
+];
+
+export default data;
