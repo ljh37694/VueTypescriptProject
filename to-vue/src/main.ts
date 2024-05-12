10f496383936d0a8c0b2a776812338037ce55e31
i_junghoon <ljh37694@naver.com>
2024-05-12 21:42:14 +0900

    fontawesome 설치

--- to-vue/src/main.ts
+++ to-vue/src/main.ts
@@ -2,4 +2,19 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
 
-createApp(App).use(store).mount('#app')
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
+const app = createApp(App)
+
+library.add(fas, far, fab);
+
+app.component('font-awesome-icon', FontAwesomeIcon)
+app.use(store).mount('#app')
