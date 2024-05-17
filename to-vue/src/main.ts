26591cb6ecaa514a58653a7f22bdd1dbee95006e
i_junghoon <ljh37694@naver.com>
2024-05-17 13:50:25 +0900

    부트스트랩 설치

--- to-vue/src/main.ts
+++ to-vue/src/main.ts
@@ -12,6 +12,9 @@ import { fas } from '@fortawesome/free-solid-svg-icons'
 import { far } from '@fortawesome/free-regular-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 const app = createApp(App)
 
 library.add(fas, far, fab);
