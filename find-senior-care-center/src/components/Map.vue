7e6bfe8e9cf4fd627ad59f59aef788113b3a28c9
i_junghoon <ljh37694@naver.com>
2024-05-27 18:36:49 +0900

    지도 틀만 만듦

---
+++ find-senior-care-center/src/components/Map.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="map">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainMap",
+}
+</script>
+
+<style>
+#map {
+  height: 100%;
+  flex-grow: 1;
+  position: absolute;
+  left: 80px;
+  width: calc(100% - 80px);
+  background-color: aqua;
+  z-index: 1;
+}
+</style>
