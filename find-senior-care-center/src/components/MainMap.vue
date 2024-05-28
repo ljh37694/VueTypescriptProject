01c915153405d19ada47b25feab739f209372196
i_junghoon <ljh37694@naver.com>
2024-05-28 13:45:31 +0900

    NaverMap Circle 만들어 봄

--- find-senior-care-center/src/components/MainMap.vue
+++ find-senior-care-center/src/components/MainMap.vue
@@ -1,33 +1,49 @@
 <script setup>
-import { NaverMap } from "vue3-naver-maps";
+import { NaverMap, NaverCircle } from "vue3-naver-maps";
+import { mapMutations, mapState } from "vuex";
+
+const circle = ref();
 
 const onLoadMap = (map) => {
-  let latitude = 0;
-  let longitude = 0;
-
-  navigator.geolocation.getCurrentPosition(pos => {
-    latitude = pos.coords.latitude;
-    longitude = pos.coords.longitude;
-
-
-    const latLng = new window.naver.maps.LatLng(latitude, longitude); // window 생략 가능
-    map.panTo(latLng); // Change Map Center
-  });
+  const latLng = new window.naver.maps.LatLng(37.51347, 127.041722); // window 생략 가능
+  map.panTo(latLng); // Change Map Center
 };
 
+const onLoadCircle = (circleObject) => {
+  circle.value = circleObject;
+};
 </script>
 
 <template>
   <div id="map">
-    <naver-map style="width: 100%; height: 100%" @onLoad="onLoadMap($event)" />
+    <naver-map style="width: 100%; height: 100%" @onLoad="onLoadMap($event)" ref="MainMap" :mapOptions="mapOptions">
+      <naver-circle :latitude="37.51347" :longitude="127.041722" :radius="350" @onLoad="onLoadCircle($event)"
+        ref="cicle" />
+    </naver-map>
   </div>
 </template>
 
 <script>
+import { ref } from "vue";
+
 export default {
   name: "MainMap",
+  computed: {
+    ...mapState(['curGeolocation', 'mapOptions']),
+  },
+  methods: {
+    ...mapMutations(['setCurGeolocation']),
+  },
   mounted() {
-  }
+  },
+  beforeMount() {
+    navigator.geolocation.getCurrentPosition(pos => {
+      this.setCurGeolocation({
+        latitude: pos.coords.latitude,
+        longitude: pos.coords.longitude,
+      });
+    });
+  },
 }
 </script>
 
--- find-senior-care-center/src/store.js
+++ find-senior-care-center/src/store.js
@@ -5,12 +5,26 @@ const store = createStore({
   state () {
     return {
       activeNavMenu: 0,
+      curGeolocation: {
+        latitude: 0,
+        longitude: 0,
+      },
+      mapOptions: {
+        latitude: 37.51347, // 지도 중앙 위도
+        longitude: 127.041722, // 지도 중앙 경도
+        zoom: 13,
+        zoomControl: false,
+        zoomControlOptions: { position: "TOP_RIGHT" },
+      },
     }
   },
   mutations: {
     changeActiveNavMenu(state, index) {
       state.activeNavMenu = index;
-    }
+    },
+    setCurGeolocation(state, geolocation) {
+      state.curGeolocation.latitude = geolocation;
+    },
   }
 });
 
