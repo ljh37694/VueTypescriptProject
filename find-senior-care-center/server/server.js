b975ef7b9bdf74cbbd49fe2dd40d5e747f2065a1
i_junghoon <ljh37694@naver.com>
2024-05-30 22:26:46 +0900

    vue CORS 에러 해결하려고 함

--- find-senior-care-center/server/server.js
+++ find-senior-care-center/server/server.js
@@ -7,21 +7,21 @@ require("dotenv").config();
 
 app.listen(port, () => {
   console.log('http://localhost:3000 에서 서버 실행 중');
+  console.log("https://openapi.naver.com/v1/search/local.json?query=" + encodeURI('경기도 광주시 주간보호센터') + "&display=10");
 });
 
-app.get('/', (req, res) => {
-  res.send("Hello World!!!");
-
-  axios({
+app.get('/', async (req, res) => {
+  await axios({
     method: "get",
-    url: "https://openapi.naver.com/v1/search/local.json?query=" + encodeURI('경복궁') + "&display=5",
+    url: "https://openapi.naver.com/v1/search/local.json?query=" + encodeURI('경덕재주간보호센터') + "&display=5&sort=comment",
     headers: {
       "X-Naver-Client-Id": process.env.VUE_APP_NAVER_CLIENT_ID,
       "X-Naver-Client-Secret": process.env.VUE_APP_NAVER_CLIENT_SECRET
     }
   })
-  .then(res => {
-    console.log(res.data);  
+  .then(response => {
+    console.log(response.data);
+    res.send(response.data);
   })
   .catch(e => console.log(e))
 });
